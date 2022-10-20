29a3cfcff7cb848722aa9f08f0e0b01356c63182
BoryaKuu <paramonovamasha02@gmail.com>
2022-10-20 14:13:34 +1000

    provercall

---
+++ proverca1.py
@@ -0,0 +1,19 @@
+f=open(r'C:\compilator\01.in', 'r')
+for line in f:
+    print(line)
+
+c=open(r'C:\compilator\01.out', 'r')
+for line in c:
+    print(line)
+
+
+if f!='':
+    print('01')
+if c!='':
+    print('02')
+f.close
+c.close
+
+
+
+
